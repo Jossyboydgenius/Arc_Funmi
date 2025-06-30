51246e2e9b318a33d2e3f7e2c6548f229e67f6c7
jossyboydgenius <jossyboydgenius@gmail.com>
2025-06-30 17:26:40 +0000

    Add Featured section to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import WhyArcFunmi from "@/components/sections/WhyArcFunmi";
+import Featured from "@/components/sections/Featured";
 import HotTopics from "@/components/sections/HotTopics";
 import CallToAction from "@/components/sections/CallToAction";
 import Newsletter from "@/components/sections/Newsletter";
@@ -12,6 +13,7 @@ export default function Home() {
       <Header />
       <HeroSection />
       <WhyArcFunmi />
+      <Featured />
       <HotTopics />
       <CallToAction />
       <Newsletter />
